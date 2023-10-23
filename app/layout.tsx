542839bb464257e374f04fa0f82ffc528f30a82b
pdro-h0 <pedro.pho792@gmail.com>
2023-10-23 17:17:56 -0300

    Fazendo o SearchInput

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body>
+        <body className={cn("bg-secondary", inter.className)}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
